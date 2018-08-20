823b4531c2794ba5bf04aedf5947ab32cf7af81d
Brian Wilhite <bcwilhite@live.com>
2018-08-20 16:14:15 -0400

    commit for appveyor

--- Tests/PendingReboot.tests.ps1
+++ Tests/PendingReboot.tests.ps1
@@ -14,7 +14,7 @@ Describe "General project validation: $moduleName" {
 
     $scripts = Get-ChildItem -Path $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
-    # TestCases are splatted to the script so we need hashtables
+    # TestCases are splatted to the script so we will need hashtables
     $testCase = $scripts | Foreach-Object {@{file = $_}}
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
